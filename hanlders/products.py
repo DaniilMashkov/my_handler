21c7d52f14d4c2c3e5d0a3c3d867e1f0a08f9df8
Константин Шамонов <razlionx@yandex.ru>
2022-11-25 21:17:43 +0700

    создал функцию get_single_product в файле product.py

--- hanlders/products.py
+++ hanlders/products.py
@@ -1,6 +1,23 @@
 def get_product_list(data):
     pass
-
-
+import json
 def get_single_product(data):
-    pass
+  with open('../data/catalog.json') as product_file:
+    file_data = json.load(product_file) 
+    user_product_id = data['data']['id']
+    code = 404
+    message = "Товара с таким номером не найдено"
+    for category in file_data:
+        for product in category['products']:
+            if user_product_id == product['id']:
+                code = 200
+                message = f"{product['name']}\nЦена: {product['price']} рублей за {product['unit']}\nОстаток на складе:{product['balance']}{product['unit']}\nОписание:{product['description']}"
+    
+    return {'code': code, 'message': message}
+
+get_single_product({
+        "action": 4,
+            "data": {
+                      "id": 1
+                          }
+            })
