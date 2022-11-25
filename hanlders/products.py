2aea1ab9c51107beba2977b218a93290aeb734c1
DaniilMashkov <dankeeeez@gmail.com>
2022-11-25 23:27:32 +0700

    set_function(get_product_list)

--- hanlders/products.py
+++ hanlders/products.py
@@ -1,6 +1,39 @@
+import json
+data = {
+    "action": 2,
+    "filter": {
+        "price": None,
+        "category": 2
+    }
+}
 def get_product_list(data):
-    pass
 
+    with open('../data/catalog.json') as catalog:
+        file_data = catalog.read()
+        file = (json.loads(file_data))
+
+        f_price = data['filter']['price']
+        f_category = data['filter']['category']
+        all_goods = [x['products'] for x in file]
+
+        for cat in file:
+            if cat['id'] == f_category:
+                if f_price is None:
+                    return [print(x) for x in cat['products']]
+                elif '>' or '<'  or'=' in f_price:
+                    for i in all_goods:
+                        for k in i:
+                            filter_price = (eval(str(k["price"])+str(f_price)))
+                            if filter_price: print(k)
+
+                else:
+                    [print(x) for x in cat['products']]
+
+
+
+
+
+get_product_list(data)
 
 def get_single_product(data):
     pass
