f882f52eab2e91fc9506d5d82e12a827d0c5fa8a
Константин Шамонов <razlionx@yandex.ru>
2022-11-28 09:06:40 +0700

    измени путь к дате

--- hanlders/products.py
+++ hanlders/products.py
@@ -2,7 +2,7 @@ def get_product_list(data):
     pass
 import json
 def get_single_product(data):
-  with open('../data/catalog.json') as product_file:
+  with open('data/catalog.json') as product_file:
     file_data = json.load(product_file) 
     user_product_id = data['data']['id']
     code = 404
@@ -14,10 +14,3 @@ def get_single_product(data):
                 message = f"{product['name']}\nЦена: {product['price']} рублей за {product['unit']}\nОстаток на складе:{product['balance']}{product['unit']}\nОписание:{product['description']}"
     
     return {'code': code, 'message': message}
-
-get_single_product({
-        "action": 4,
-            "data": {
-                      "id": 1
-                          }
-            })
--- tests/test_get_single_product.py
+++ tests/test_get_single_product.py
@@ -1,4 +1,3 @@
 from hanlders.products import get_single_product 
 def test_get_single_product():
-    
-    
+
